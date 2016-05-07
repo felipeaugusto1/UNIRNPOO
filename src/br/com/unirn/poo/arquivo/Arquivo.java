d0a9b3adc5209eed83c9d3ef79f6aa6b4ac40281
allan <allan@allan-PC>
2016-05-07 16:58:22 -0300

    Começo da classe que implementa as leitura e salvamento dos arquivos.

---
+++ src/br/com/unirn/poo/arquivo/Arquivo.java
@@ -0,0 +1,23 @@
+package br.com.unirn.poo.arquivo;
+
+import br.com.unirn.poo.modelo.Usuario;
+import br.com.unirn.poo.singleton.ListasSingleton;
+
+/**
+ * Classe para manipular arquivos que aramazenarão os dados gerado no sistema.
+ *
+ * @author allan
+ *
+ */
+public class Arquivo {
+	
+	private String localArquivo;
+	
+	public static void salvarArquvio(){
+		
+	}
+	
+	public static void lerArquivo(){
+		
+	}
+}
