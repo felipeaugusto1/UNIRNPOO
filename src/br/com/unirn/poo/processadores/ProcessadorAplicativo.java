e1e1b025cb9727f38ffdca96391d72db447b9a23
allan <allan@allan-PC>
2016-05-07 16:37:09 -0300

    ProcessadorAplicativo que será implementado por ramon seguindo o  padrão definido no processadorGeneric.

---
+++ src/br/com/unirn/poo/processadores/ProcessadorAplicativo.java
@@ -0,0 +1,21 @@
+package br.com.unirn.poo.processadores;
+
+import java.util.Collection;
+
+import br.com.unirn.poo.modelo.Aplicativo;
+
+public class ProcessadorAplicativo extends  ProcessadorGeneric<Aplicativo>{
+
+	@Override
+	public void cadastrar(Aplicativo obj, Collection<Aplicativo> listaDeObjetos) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean validate(Aplicativo obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
